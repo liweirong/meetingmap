73064f1636a3895e8a0fad46533552056cc93e24
lwrong <2277839278@qq.com>
2018-02-06 09:31:41 +0800

    逆向工程生成UserInfo的mapper等

---
+++ myProject/src/main/java/com/iris/service/IUserInfoService.java
@@ -0,0 +1,7 @@
+package com.iris.service;
+
+/**
+ * Created by lwrong on 2018/2/6.
+ */
+public interface IUserInfoService {
+}
---
+++ myProject/src/test/java/test/UserInfoMapperTest.java
@@ -0,0 +1,70 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by lwrong on 2018/2/6.
+ */
+public class UserInfoMapperTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testCountByExample() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteByExample() throws Exception {
+
+    }
+
+    @Test
+    public void testDeleteByPrimaryKey() throws Exception {
+
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+
+    }
+
+    @Test
+    public void testInsertSelective() throws Exception {
+
+    }
+
+    @Test
+    public void testSelectByExample() throws Exception {
+
+    }
+
+    @Test
+    public void testSelectByPrimaryKey() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateByExampleSelective() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateByExample() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateByPrimaryKeySelective() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateByPrimaryKey() throws Exception {
+
+    }
+}
